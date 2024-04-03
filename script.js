d9ab09eb5864bd1a3653dc13f775e22514df25cf
fatah <fatahutd@gmailcom>
2024-04-03 12:20:38 +0200

    begin bonus modofication

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-let array = [
-    { title: "imgGit", image: "./assets/git.png" },
-    { title: "imgHtml", image: "./assets/html.png" },
-    { title: "imgJs", image: "./assets/js.png" },
-    { title: "imgNode", image: "./assets/node.png" },
-    { title: "imgPhp", image: "./assets/php.png" },
-    { title: "imgReact", image: "./assets/react.png" },
-    { title: "imgSymfony", image: "./assets/symfony.png" },
-    { title: "imgVue", image: "./assets/vue.png" }
+let array = [{
+        title: "imgGit",
+        image: "./assets/git.png"
+    },
+    {
+        title: "imgHtml",
+        image: "./assets/html.png"
+    },
+    {
+        title: "imgJs",
+        image: "./assets/js.png"
+    },
+    {
+        title: "imgNode",
+        image: "./assets/node.png"
+    },
+    {
+        title: "imgPhp",
+        image: "./assets/php.png"
+    },
+    {
+        title: "imgReact",
+        image: "./assets/react.png"
+    },
+    {
+        title: "imgSymfony",
+        image: "./assets/symfony.png"
+    },
+    {
+        title: "imgVue",
+        image: "./assets/vue.png"
+    }
 ];
-let btn = document.querySelector("#btn");
+// Récupiration des element du DOM
+let btn = document.querySelector("#load");
+let save = document.querySelector("#save");
+let reprise = document.querySelector("#reprise");
+let countContainer = document.querySelector("#click");
+let timeContainer = document.querySelector("#time");
+console.log(timeContainer)
+
+// supprimer le storage à chauqe recharge du la page
+localStorage.removeItem("count")
 
 // Dupliquer chaque élément de la liste une fois
 array = array.flatMap(item => [item, item]);
 
-let container = document.querySelector(".big_container");  
+let container = document.querySelector(".big_container");
 let flippedCards = []; // Variable pour stocker les chemins d'image des cartes retournées
 
 // Mélanger les cartes
@@ -25,9 +57,9 @@ array.forEach((element) => {
     newElement.classList.add("imageElement");
     newElement.setAttribute("src", "./assets/default.png");
     newElement.dataset.image = element.image; // Stocker le chemin d'image dans un attribut de données
-    container.appendChild(newElement); 
+    container.appendChild(newElement);
     // Ajouter un gestionnaire d'événements de clic à chaque carte
-    newElement.addEventListener("click", () => { 
+    newElement.addEventListener("click", () => {
         // Vérifier si moins de deux cartes sont déjà retournées et si la carte n'est pas déjà retournée
         if (flippedCards.length < 2 && !newElement.classList.contains("imageFind")) {
             // Retourner la carte en changeant son apparence
@@ -39,7 +71,7 @@ array.forEach((element) => {
             if (flippedCards.length === 2) {
                 const [firstCard, secondCard] = flippedCards;
                 // operateur ternaire
-                (firstCard.dataset.image === secondCard.dataset.image) ?flippedCards = [] : setTimeout(() => {
+                (firstCard.dataset.image === secondCard.dataset.image) ? flippedCards = []: setTimeout(() => {
                     flippedCards.forEach(card => {
                         card.setAttribute("src", "./assets/default.png");
                         card.classList.remove("imageFind");
@@ -47,9 +79,33 @@ array.forEach((element) => {
                     flippedCards = []; // Vider flippedCards
                 }, 1000);
             }
+
         }
+
+        // compteur de click
+        count = localStorage.getItem("count")
+        count++
+        localStorage.setItem("count", count)
+        countContainer.textContent = count;
+        console.log(count)
     });
 });
-btn.addEventListener("click", ()=>{
+
+// bouton de rechargement de la page
+btn.addEventListener("click", () => {
     location.reload();
+    localStorage.removeItem("count")
+});
+
+//bouton de sauvgarde de la partie
+save.addEventListener("click", () => {
+    const containerContent = container.innerHTML;
+    localStorage.setItem("gameSaved", containerContent);
+});
+
+//bouton de reprise de la partie
+reprise.addEventListener("click", () => {
+    const savedContent = localStorage.getItem("gameSaved");
+    container.innerHTML = savedContent;
+
 });
